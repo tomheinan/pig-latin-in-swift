962616002f5b6994ad7f3827b5bd4896f7d2a720
Tom Heinan <theinan@onemedical.com>
2014-07-05 12:59:10 -0700

    Initial commit

---
+++ PigLatin/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PigLatin
+//
+//  Created by Tom Heinan on 7/5/14.
+//  Copyright (c) 2014 Tom Heinan. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
