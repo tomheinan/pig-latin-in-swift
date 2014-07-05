53193bbceb48d27024ce37d1b24bd675a033b6ea
Tom Heinan <theinan@onemedical.com>
2014-07-05 13:26:57 -0700

    Added an empty extension on String for piggify and the requisite test cases

---
+++ PigLatin/String+PigLatin.swift
@@ -0,0 +1,15 @@
+//
+//  String+PigLatin.swift
+//  PigLatin
+//
+//  Created by Tom Heinan on 7/5/14.
+//  Copyright (c) 2014 Tom Heinan. All rights reserved.
+//
+
+extension String {
+	
+	func piggify() -> String {
+		return "foo"
+	}
+	
+}
